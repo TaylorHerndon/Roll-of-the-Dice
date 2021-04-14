93a3d5b7e9595e33cd0976cc9239bb7529c8cbf4
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-04-13 22:15:31 -0600

    Changed variable casing to camelCasing Removed variable "i" Removed uneccesary line spacing Changed how rolls were made Changed how spacer lines were written

--- RollOfTheDice.vb
+++ RollOfTheDice.vb
@@ -14,82 +14,63 @@ Module RollOfTheDice
 
         Randomize()
 
-        Dim RollA As Integer = Nothing
-        Dim RollB As Integer = Nothing
-        Dim Result As Integer = Nothing
-        Dim i As Integer = 0
-        Dim LogRoll(999) As Integer
-        Dim RollRecursions(12) As Integer
+        Dim rollA As Integer = Nothing
+        Dim rollB As Integer = Nothing
+        Dim result As Integer = Nothing
+        Dim logRoll(999) As Integer
+        Dim rollRecursions(12) As Integer
 
         'Make two rolls, add them together, and log the result
         For i = 0 To 999
+            rollA = CInt(Math.Ceiling(VBMath.Rnd * 6))
+            rollB = CInt(Math.Ceiling(VBMath.Rnd * 6))
 
-            Do Until RollA <> 0 And RollB <> 0
+            result = rollA + rollB
 
-                RollA = Convert.ToInt32(VBMath.Rnd * 6)
-                RollB = Convert.ToInt32(VBMath.Rnd * 6)
-
-            Loop
-
-            Result = RollA + RollB
-
-            LogRoll(i) = Result
-
-            RollA = 0
-            RollB = 0
-
+            logRoll(i) = result
         Next
-
-        i = 0
 
         'Go through the log and count and index how many times each number occoured
         For i = 0 To 999
 
-            Select Case LogRoll(i)
+            Select Case logRoll(i)
                 Case 2
-                    RollRecursions(2) = RollRecursions(2) + 1
+                    rollRecursions(2) = rollRecursions(2) + 1
                 Case 3
-                    RollRecursions(3) = RollRecursions(3) + 1
+                    rollRecursions(3) = rollRecursions(3) + 1
                 Case 4
-                    RollRecursions(4) = RollRecursions(4) + 1
+                    rollRecursions(4) = rollRecursions(4) + 1
                 Case 5
-                    RollRecursions(5) = RollRecursions(5) + 1
+                    rollRecursions(5) = rollRecursions(5) + 1
                 Case 6
-                    RollRecursions(6) = RollRecursions(6) + 1
+                    rollRecursions(6) = rollRecursions(6) + 1
                 Case 7
-                    RollRecursions(7) = RollRecursions(7) + 1
+                    rollRecursions(7) = rollRecursions(7) + 1
                 Case 8
-                    RollRecursions(8) = RollRecursions(8) + 1
+                    rollRecursions(8) = rollRecursions(8) + 1
                 Case 9
-                    RollRecursions(9) = RollRecursions(9) + 1
+                    rollRecursions(9) = rollRecursions(9) + 1
                 Case 10
-                    RollRecursions(10) = RollRecursions(10) + 1
+                    rollRecursions(10) = rollRecursions(10) + 1
                 Case 11
-                    RollRecursions(11) = RollRecursions(11) + 1
+                    rollRecursions(11) = rollRecursions(11) + 1
                 Case 12
-                    RollRecursions(12) = RollRecursions(12) + 1
+                    rollRecursions(12) = rollRecursions(12) + 1
             End Select
 
         Next
 
         'Write out the header and the results from the RollRecursions array
-
         Console.WriteLine("                           Roll of the Dice                        ")
-        Console.WriteLine("-------------------------------------------------------------------")
+        Console.WriteLine(StrDup(67, "-"))
         Console.WriteLine("|  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10 |  11 |  12 |")
-        Console.WriteLine("-------------------------------------------------------------------")
-        Console.Write("|" & RollRecursions(2).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(3).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(4).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(5).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(6).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(7).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(8).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(9).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(10).ToString.PadLeft(4) & " ")
-        Console.Write("|" & RollRecursions(11).ToString.PadLeft(4) & " ")
-        Console.WriteLine("|" & RollRecursions(12).ToString.PadLeft(4) & " |")
-        Console.WriteLine("-------------------------------------------------------------------")
+        Console.WriteLine(StrDup(67, "-"))
+
+        For i = 2 To 12
+            Console.Write("|" & rollRecursions(i).ToString.PadLeft(4) & " ")
+        Next
+
+        Console.WriteLine("|" & vbNewLine & StrDup(67, "-"))
 
         Console.WriteLine(vbNewLine & "[Press any key to exit]")
         Console.ReadKey()
